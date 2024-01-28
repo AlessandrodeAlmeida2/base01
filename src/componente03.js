03184834682dabd0f6a53e57d2f11a20de3f6289
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-01-28 08:42:52 -0300

    ultima aula

--- src/componente03.js
+++ src/componente03.js
@@ -4,7 +4,11 @@ class Componente03 extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {cor: 'vermelho'}
+        this.state = {
+            cor: 'vermelho',
+            nome: '',
+            idade: null
+        }
     }
 
     // Depois de realizar o render
@@ -14,8 +18,31 @@ class Componente03 extends React.Component {
         }, 2000)
     }
 
+    minhaFuncao = (elemento) => {
+        elemento.preventDefault(); // Não permiti que o action do onSubimit funcione
+        alert('Testando evento onSubmit');
+    }
+
+    enviarParaState = (campo) => {
+        this.setState({[campo.target.name]: campo.target.value});
+    }
+
+
     render() {
-        return <h1>Minha cor preferida é o {this.state.cor}</h1>
+        return (
+            <div>
+                <h1>Minha cor preferida é o {this.state.cor}</h1>
+                <form>
+                    <input type="text" placeholder="Nome" name="nome" onChange={this.enviarParaState} />
+                    <input type="number" placeholder="Idade" name="idade" onChange={this.enviarParaState} />
+                    <p>{this.state.nome}</p>
+                    <p>{this.state.idade}</p>
+                </form>
+                <form onSubmit={this.minhaFuncao} action="https://alessandrodealmeida.dev.br/">
+                    <input type="submit" />
+                </form>
+            </div>
+        )
     }
 }
 
